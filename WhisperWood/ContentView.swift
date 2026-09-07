f99ee3f9ceb09895522be10d74a3813f55ac60db
Nathanael Wilson Angelo <nathanaelwilsonangelo@gmail.com>
2026-09-07 10:44:19 +0700

    Initial Commit

---
+++ WhisperWood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhisperWood
+//
+//  Created by Nathanael Wilson Angelo on 07/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhisperWood/WhisperWoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperWoodApp.swift
+//  WhisperWood
+//
+//  Created by Nathanael Wilson Angelo on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct WhisperWoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
